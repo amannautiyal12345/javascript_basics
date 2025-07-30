7de0778251496f5ee46035519720dd53e82ee42b
amannautiyal12345 <168369642+amannautiyal12345@users.noreply.github.com>
2025-07-30 16:58:32 +0000

    haha just starting

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 1214214
+let accountEmail = "amannautiyal777@gmail.com"
+var accountPassword = "ajijdisj"
+accountCity = "Jaipur"
+let accountState;
+//  (undefined)
+
+
+//  Not Allowed -accountId = 2 
+// console.log(accountId);
+accountEmail="amannan"
+accountPassword="ajsljsa"
+accountCity = "Bengaluru"
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
+
+
+/* 
+Prefer not to use "var" bcuz of issue in block scope and functional scope
+*/
