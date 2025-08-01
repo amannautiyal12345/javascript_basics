1bd73e7fecb0ae91066810bb0533f4e0bb9f3e80
amannautiyal12345 <168369642+amannautiyal12345@users.noreply.github.com>
2025-08-01 23:01:04 +0530

    02_datatypes.js
    just learned the datatypes in javascript

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,38 @@
+"use strict"; // treat all js code as newer version 
+
+// alert (3+3) // we are using nodejs , not browser
+
+console.log(3
+    +
+    3)//code readability should be high
+
+    console.log("aman")
+
+    //1)tc39.es 2)mdn
+
+    let name = "aman"
+    let age = 18
+    let isLoggedIn =  false
+    let state;
+
+
+    // Primitives
+    // number => 2 to the power 53 (range)
+    // bigint
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined =>
+    // symbol => unique 
+
+
+
+ //object
+
+ console.log(typeof "aman");  // string
+ 
+ console.log(typeof age ); // number
+
+ console.log(typeof undefined); // undefined
+ 
+ console.log(typeof null); //object
