e7960225f633ef64cd41b8a31cf831571c817b74
Aman Nautiyal <168369642+amannautiyal12345@users.noreply.github.com>
2025-09-09 11:37:35 +0000

    Now we are learning control flows

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,56 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+
+//  comparision operator (<, >, <=, >=, ==, !=, ===, !==)
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);   // User power : fly
+// }
+
+// console.log(`User power: ${power}`);    // block scope
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2"); // implicit scope
+// (so immature format)
+// if (balance > 500) console.log("test");
+
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 900");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
